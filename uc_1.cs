2caa0edecefd19c28fdba7a438481dc77d9bd387
anasalagar <90851784+anasalagar@users.noreply.github.com>
2021-10-13 11:44:54 +0530

    Update uc_1.cs

--- uc_1.cs
+++ uc_1.cs
@@ -1,64 +1,49 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace practice
 
-namespace ConsoleApp22
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
             Random r = new Random();
-            int flip = r.Next(0, 2);
-
-            int head = 0;
-            int tails = 0;
             int i = 0;
+            double head = 0;
+            double tails = 0;
+
             double PerOfHead;
             double PerOfTails;
+            Console.WriteLine("<<<<Welcome to coin flip game>>>>\n");
 
-
-            Console.WriteLine("<<<<Welcome to coin flip game>>>>");
-
-            Console.WriteLine("how many time you wanna flip the coin ?");
+            Console.Write("how many time you wanna flip the coin ? - ");
             int N = Convert.ToInt32(Console.ReadLine());
 
-
-            while (i!= N)
+            while (i != N)
             {
-                flip =r.Next(2);
-                if (flip==0)
+                int flip = r.Next(2);
+
+                if (flip == 0)
                 {
-                    
-                    Console.WriteLine("The toss number {0} outcome is : Heads", i + 1);
+                    Console.WriteLine("The toss number {0} outcome is : Heads", i+1);
                     head++;
                 }
                 else
                 {
-                   
-                    Console.WriteLine("The toss number {0} outcome is : Tails", i + 1);
+                    Console.WriteLine("The toss number {0} outcome is : Tails", i+1);
                     tails++;
                 }
                 i++;
             }
 
-            Console.WriteLine("numbers of tails are : " + tails);
-            Console.ReadLine();
-            Console.WriteLine("numbers of heads are : " + head);
-            Console.ReadLine();
+            Console.WriteLine("\nNumbers of Tails are : " + tails);
+            Console.WriteLine("Numbers of Heads are : " + head);
 
             PerOfHead = head / N * 100;
             PerOfTails = tails / N * 100;
 
-            Console.WriteLine("Percentage of heads is " + PerOfHead);
-            Console.ReadLine();
+            Console.WriteLine("\nPercentage of heads is " + PerOfHead);
             Console.WriteLine("percentage of tails is " + PerOfTails);
             Console.ReadLine();
-
-
         }
     }
 }
